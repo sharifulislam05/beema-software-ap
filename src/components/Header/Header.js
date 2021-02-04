448ed87b6a57b29e513bc7d9090dbc0f6a7a6c17
smshorif52 <smshorif52@gmail.com>
2021-02-04 10:39:06 +0600

    error fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import headerStyles from "../../../styles/Header.module.scss";
+import headerStyles from "../../../styles/header.module.scss";
 
 const Header = () => {
   return (
